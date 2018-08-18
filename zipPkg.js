0de16524cf377da1bacbeda62f19d4653907a9c3
jameswilddev <jameswilddev@outlook.com>
2018-08-18 22:05:30 +0100

    Binaries are now zipped.

---
+++ zipPkg.js
@@ -0,0 +1,36 @@
+import * as fs from "fs"
+import * as path from "path"
+import recursiveReaddir from "recursive-readdir"
+import archiver from "archiver"
+
+recursiveReaddir(`dist`, (error, files) => {
+  if (error) {
+    throw error
+  }
+
+  for (const file of files) {
+    if (file.endsWith(`.js`)) {
+      continue
+    }
+
+    const archive = archiver(`zip`)
+    archive.append(fs.createReadStream(file), { name: path.relative(`dist`, file) })
+    const output = fs.createWriteStream(`${file}.zip`)
+    output.on(`error`, error => {
+      throw error
+    })
+    output.on(`close`, () => {
+      fs.unlink(file, error => {
+        if (error) {
+          throw error
+        }
+      })
+    })
+    archive.pipe(output)
+    archive.on(`warning`, error => console.warn(error))
+    archive.on(`error`, error => {
+      throw error
+    })
+    archive.finalize()
+  }
+})
