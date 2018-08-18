b49ee730b33c2a16200e731fcde9016fcbe98a07
jameswilddev <jameswilddev@outlook.com>
2018-08-18 13:14:43 +0100

    Initial commit; currently just parses command line arguments.

---
+++ readme.md
@@ -0,0 +1,6 @@
+# vnhtml
+
+A command-line application which generates HTML/CSS visual novels from a basic
+scripting language.
+
+Made as an esoteric entry to [JS13kGames](https://js13kgames.com).
---
+++ src/vnhtml.js
@@ -0,0 +1,49 @@
+
+import commander from "commander"
+
+const command = commander
+  .version(`0.0.0`)
+  .description(`Generates HTML/CSS visual novels from a basic scripting language`)
+  .option(`-w --watch`, `recompile on changes`)
+  .option(`-s --script [path]`, `the script to parse`)
+  .option(`-h --html [path]`, `write a html file`)
+  .option(`-z --zip [path]`, `write a zip file`)
+  .parse(process.argv)
+
+switch (command.script) {
+  case undefined:
+    if (!command.args.length) {
+      console.error(`Missing script path`)
+      process.exit(1)
+    } else if (command.args.length > 1) {
+      console.error(`Multiple script paths: ${command.args.map(arg => JSON.stringify(arg)).join(`, `)}`)
+      process.exit(1)
+    }
+    command.script = command.args[0]
+    break
+
+  case false:
+  case true:
+    console.error(`Missing script path`)
+    process.exit(1)
+
+  default:
+    if (command.args.length) {
+      console.error(`Unexpected arguments: ${command.args.map(arg => JSON.stringify(arg)).join(`, `)}`)
+      process.exit(1)
+    }
+}
+
+switch (command.html) {
+  case false:
+  case true:
+    console.error(`Missing html path`)
+    process.exit(1)
+}
+
+switch (command.zip) {
+  case false:
+  case true:
+    console.error(`Missing zip path`)
+    process.exit(1)
+}
