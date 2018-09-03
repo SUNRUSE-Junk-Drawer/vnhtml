5225eb5016e1b5ddce7449200d59ecf89ac58e67
jameswilddev <jameswilddev@outlook.com>
2018-09-03 22:20:04 +0100

    0.0.20

--- tools/synchronizeVersions.js
+++ tools/synchronizeVersions.js
@@ -1,7 +1,7 @@
 import * as path from "path"
 import * as fs from "fs"
 
-const version = `0.0.19`
+const version = `0.0.20`
 
 fs.readdir(path.join(`..`, `packages`), (error, files) => {
   if (error) {
